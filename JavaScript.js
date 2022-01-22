e51a0153c58755fc39b82af68881d043fd430bec
hudsoncap <hud.cardoso@hotmail.com>
2022-01-22 17:08:11 -0300

    SiteEstudos
    estudando.

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+       function Avaliar(){
+       let nota = window.prompt('Nota de 1 a 5')    
+       
+        if (nota >= 1 && nota <= 5 ) {
+              alert(`Sua Nota: ${nota}`)
+              alert('Obrigado!!!')
+        } 
+         else {
+              alert('[ERRO] Valor Invalido!!!')
+        }
+    } 
+      
